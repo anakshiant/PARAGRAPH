6066d454c58295fbbc2471a79fd229dfd48f1ff1
anakshiant <anakshiant@users.noreply.github.com>
2024-06-12 15:20:35 +0000

    ⤵️ Sync changes from remote

---
+++ src/integrations/pipedrive/config.ts
@@ -0,0 +1,19 @@
+import { IIntegrationConfig } from '@useparagon/core/integration';
+
+/**
+ * configuration for a pipedrive
+ */
+const config: IIntegrationConfig = {
+  description: 'Sync records with Pipedrive',
+  overviewText: `Connect your Pipedrive account to sync records from your Pipedrive CRM. Enable your sales team to close more deals by keeping your Pipedrive CRM records up to date - without manual data entry.
+ 
+Our Pipedrive integration enables you to:
+ 
+• Automatically create or update records like contacts or deals in Pipedrive
+• Sync records from Pipedrive
+• Receive updates when new records are created in Pipedrive`,
+  showWatermark: true,
+  workflowDisplayOrder: [],
+};
+
+export default config;
