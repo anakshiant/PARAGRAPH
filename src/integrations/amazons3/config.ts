a4798f7e01fc6ed755c7e17709dd6252249b54d7
anakshiant <anakshiant@users.noreply.github.com>
2024-07-08 11:24:29 +0000

    ⤵️ Sync changes from remote

---
+++ src/integrations/amazons3/config.ts
@@ -0,0 +1,18 @@
+import { IIntegrationConfig } from '@useparagon/core/integration';
+
+/**
+ * configuration for a amazons3
+ */
+const config: IIntegrationConfig = {
+  description: 'Connect your Amazon S3 bucket',
+  overviewText: `Connect to your Amazon S3 account to manage your buckets, objects, and jobs in Amazon S3. Increase your team’s productivity by keeping your Amazon S3 account up to date - without manual data entry.
+              
+Our Amazon S3 integration enables you to:
+           
+• Sync buckets in your Amazon S3 account 
+• Create and update buckets, objects and jobs in your Amazon S3 account`,
+  showWatermark: true,
+  workflowDisplayOrder: [],
+};
+
+export default config;
