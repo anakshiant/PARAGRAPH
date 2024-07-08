5c6e77d1411b0543a555dc02b98d4682a4708528
anakshiant <anakshiant@users.noreply.github.com>
2024-07-08 10:08:11 +0000

    ⤵️ Sync changes from remote

---
+++ src/integrations/adobeexperiencemanager/config.ts
@@ -0,0 +1,18 @@
+import { IIntegrationConfig } from '@useparagon/core/integration';
+
+/**
+ * configuration for a adobeexperiencemanager
+ */
+const config: IIntegrationConfig = {
+  description: 'Connect your AEM account',
+  overviewText: `Connect to your Adobe Experience Manager account to manage your web assets in Adobe Experience Manager. Increase your team’s productivity by keeping your Adobe Experience Manager account up to date - without manual data entry. 
+                     
+Our Adobe Experience Manager integration enables you to:
+ 
+• Create and update digital assets in your Adobe Experience Manager account
+• Search for digital assets in your Adobe Experience Manager account`,
+  showWatermark: true,
+  workflowDisplayOrder: [],
+};
+
+export default config;
