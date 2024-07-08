ee3c54990938e434f24805d05c317627b0bfc9ce
anakshiant <anakshiant@users.noreply.github.com>
2024-07-08 10:54:18 +0000

    ⤵️ Sync changes from remote

---
+++ src/integrations/adpworkforcenow/config.ts
@@ -0,0 +1,19 @@
+import { IIntegrationConfig } from '@useparagon/core/integration';
+
+/**
+ * configuration for a adpworkforcenow
+ */
+const config: IIntegrationConfig = {
+  description: 'Manage your staff in ADP Workforce Now',
+  overviewText: `Connect to your ADP Workforce Now account to manage your staffing and payroll in ADP Workforce Now.Increase your team’s productivity by keeping your ADP Workforce Now account up to date - without manual data entry. 
+                     
+Our ADP Workforce Now integration enables you to:
+                  
+• Create or update workers in your ADP Workforce Now instance
+• Manage payroll within ADP Workforce Now
+• Sync workers from ADP Workforce Now`,
+  showWatermark: true,
+  workflowDisplayOrder: [],
+};
+
+export default config;
