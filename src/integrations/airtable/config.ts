eeb1256551b94e60952d4ff18736330cdde2cb60
anakshiant <anakshiant@users.noreply.github.com>
2024-07-08 11:21:44 +0000

    ⤵️ Sync changes from remote

---
+++ src/integrations/airtable/config.ts
@@ -0,0 +1,19 @@
+import { IIntegrationConfig } from '@useparagon/core/integration';
+
+/**
+ * configuration for a airtable
+ */
+const config: IIntegrationConfig = {
+  description: 'Sync data with Airtable',
+  overviewText: `Connect to your Airtable account to manage your bases and records in Airtable. Increase your team’s productivity by keeping your Airtable account up to date - without manual data entry.  
+  
+Our Airtable integration enables you to:   
+   
+• Create or update records in your Airtable bases
+• Sync records from your Airtable bases
+• Receive webhooks when records are created or updated in your Airtable bases`,
+  showWatermark: true,
+  workflowDisplayOrder: [],
+};
+
+export default config;
