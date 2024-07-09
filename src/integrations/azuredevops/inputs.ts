b73d48fcfccbf9af2bc18995c0b4c0f21994ca8f
anakshiant <anakshiant@users.noreply.github.com>
2024-07-09 08:07:06 +0000

    ⤵️ Sync changes from remote

---
+++ src/integrations/azuredevops/inputs.ts
@@ -0,0 +1,8 @@
+import { createInputs } from '@useparagon/integrations/azuredevops';
+
+/**
+ * define inputs here which can be used across workflows
+ */
+const integrationInputs = createInputs({});
+
+export default integrationInputs;
