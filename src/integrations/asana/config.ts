aba9fd79abeb96a6e7ed597cf8f3b49aa219ed8e
anakshiant <anakshiant@users.noreply.github.com>
2024-07-09 07:59:01 +0000

    ⤵️ Sync changes from remote

---
+++ src/integrations/asana/config.ts
@@ -0,0 +1,19 @@
+import { IIntegrationConfig } from '@useparagon/core/integration';
+
+/**
+ * configuration for a asana
+ */
+const config: IIntegrationConfig = {
+  description: 'Connect to your users’ Asana accounts',
+  overviewText: `Connect to your users’ Asana account to access, create, and update their tasks or projects in Asana.  
+  
+Paragon enables you to sync data between your app and your users’ Asana accounts, for example:   
+   
+• Create or update tasks in your users’ Asana projects
+• Sync tasks from your users’ Asana projects
+• Receive webhooks when tasks are created or updated in your users’ Asana projects`,
+  showWatermark: true,
+  workflowDisplayOrder: [],
+};
+
+export default config;
