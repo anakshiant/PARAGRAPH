addb604f1e1cda71559702ce5e01805c2d74e1e6
anakshiant <anakshiant@users.noreply.github.com>
2024-07-09 13:16:59 +0000

    ⤵️ Sync changes from remote

---
+++ src/integrations/emarsys/config.ts
@@ -0,0 +1,19 @@
+import { IIntegrationConfig } from '@useparagon/core/integration';
+
+/**
+ * configuration for a emarsys
+ */
+const config: IIntegrationConfig = {
+  description: 'Connect your Emarsys account',
+  overviewText: `Connect to your Emarsys account to manage your contacts, segments, and campaigns in Emarsys. Increase your team’s productivity by keeping your Emarsys account up to date - without manual data entry.
+
+Our Emarsys integration enables you to:
+       
+• Automatically create or update records in Emarsys
+• Sync contacts and segments from Emarsys
+• Receive updates when a record in Emarsys is created or updated`,
+  showWatermark: true,
+  workflowDisplayOrder: [],
+};
+
+export default config;
