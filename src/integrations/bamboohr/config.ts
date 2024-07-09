b354988782922e95202924a419ba9ed2d407d18e
anakshiant <anakshiant@users.noreply.github.com>
2024-07-09 08:58:00 +0000

    ⤵️ Sync changes from remote

---
+++ src/integrations/bamboohr/config.ts
@@ -0,0 +1,19 @@
+import { IIntegrationConfig } from '@useparagon/core/integration';
+
+/**
+ * configuration for a bamboohr
+ */
+const config: IIntegrationConfig = {
+  description: 'Manage employees, time off, and benefits in BambooHR',
+  overviewText: `Connect to your BambooHR account to manage your employees, time off, and benefits in BambooHR. Increase your team’s productivity by keeping your BambooHR account up to date - without manual data entry.
+             
+Our BambooHR integration enables you to:
+                
+• Sync employee information in your BambooHR account
+• Create employee time off requests in your BambooHR account
+• Receive updates when employee information is updated in your BambooHR account`,
+  showWatermark: true,
+  workflowDisplayOrder: [],
+};
+
+export default config;
