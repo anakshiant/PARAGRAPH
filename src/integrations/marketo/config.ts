5c2d0bf18b67f65b04c867092050f522933a7498
anakshiant <anakshiant@users.noreply.github.com>
2024-07-10 07:50:18 +0000

    ⤵️ Sync changes from remote

---
+++ src/integrations/marketo/config.ts
@@ -0,0 +1,18 @@
+import { IIntegrationConfig } from '@useparagon/core/integration';
+
+/**
+ * configuration for a marketo
+ */
+const config: IIntegrationConfig = {
+  description: 'Sync leads to Marketo',
+  overviewText: `Connect your Marketo account to sync leads to your Lists in Marketo. Grow your business and reach more customers by automating your marketing with our Marketo integration.  
+  
+Our Marketo integration enables you to:  
+    
+• Automatically create or update leads in Marketo
+• Add leads to lists in Marketo`,
+  showWatermark: true,
+  workflowDisplayOrder: [],
+};
+
+export default config;
