a9c3d4afe3a1bc2b52b809dad71dde599bd28eac
anakshiant <anakshiant@users.noreply.github.com>
2024-09-23 15:14:21 +0000

    ⤵️ Sync changes from remote

---
+++ src/integrations/jira/config.ts
@@ -0,0 +1,19 @@
+import { IIntegrationConfig } from '@useparagon/core/integration';
+
+/**
+ * configuration for a jira
+ */
+const config: IIntegrationConfig = {
+  description: 'Sync issues to Jira',
+  overviewText: `Connect your Jira Software account to create or update issues in your Jira projects. Increase your team’s productivity by keeping your Jira projects and issues up to date - without manual data entry.
+         
+Our Jira integration enables you to:
+       
+• Automatically create or update issues in Jira
+• Sync issues from Jira
+• Receive updates when issues are created or updated in Jira`,
+  showWatermark: true,
+  workflowDisplayOrder: [],
+};
+
+export default config;
